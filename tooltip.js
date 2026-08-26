b927976db23c6217be4407441ac0c0f08d0c9ed4
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-08-26 18:38:10 +0100

    Using disconnectedCallback

--- tooltip.js
+++ tooltip.js
@@ -2,6 +2,7 @@ class Tooltip extends HTMLElement {
   constructor() {
     super()
     this._tooltipContainer
+    this._tooltipIcon
     this._tooltipText = 'Some dummy tooltip text.'
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.innerHTML = `
@@ -48,9 +49,15 @@ class Tooltip extends HTMLElement {
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text')
     }
-    const tooltipIcon = this.shadowRoot.querySelector('span')
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+    this._tooltipIcon = this.shadowRoot.querySelector('span')
+    this._tooltipIcon.addEventListener(
+      'mouseenter',
+      this._showTooltip.bind(this),
+    )
+    this._tooltipIcon.addEventListener(
+      'mouseleave',
+      this._hideTooltip.bind(this),
+    )
     this.style.position = 'relative'
   }
 
@@ -64,10 +71,14 @@ class Tooltip extends HTMLElement {
     }
   }
 
-  disconnectedCallback() {}
-
   static get observedAttributes() {
     return ['text']
+  }
+
+  disconnectedCallback() {
+    console.log('Disconnected!')
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip)
+    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip)
   }
 
   _showTooltip() {
